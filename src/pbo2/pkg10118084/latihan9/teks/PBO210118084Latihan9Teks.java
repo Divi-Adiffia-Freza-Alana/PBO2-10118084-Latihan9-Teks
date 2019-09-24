e76a4f46123c264ae98210d567cead38147b9226
Divi-Adiffia-Freza-Alana <55723716+Divi-Adiffia-Freza-Alana@users.noreply.github.com>
2019-09-24 13:35:55 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118084/latihan9/teks/PBO210118084Latihan9Teks.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118084.latihan9.teks;
+
+/**
+ *
+ * @author
+ * Nama  : Divi Adiffia Freza Alana
+*  kelas : PBO2
+*  Nim   : 10118084
+*  Deskripsi Program : Program ini berisi program untuk menampilkan Teks
+
+ */
+public class PBO210118084Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab ='\t';
+        char newline ='\n';
+        c ='A';
+        System.out.println("Hasil"+newline +"char: " + tab + "berisi " + c);
+    }
+    
+}
